8b79f8f1ca6addaa2b1a6d6df0857b1183b39857
irushoka-lang <irushoka@macalester.edu>
2025-09-08 11:13:22 -0500

    Activity 2

--- Banana.py
+++ Banana.py
@@ -1,4 +1,26 @@
 banana_length = 15.5
 num_bananas = 5100
-meters_covered = banana_length * num_bananas
+length_covered = banana_length * num_bananas
+meters_covered = length_covered /100
 print(meters_covered)
+
+'Hi there'
+"Hi there"
+"I contain an apostrophe, don't I?"
+'I was told, "Double quotes go inside single quotes!"'
+s = 'hi mom'
+longstr = """As I was going to St. Ives, I met a man with seven wives.
+Each wife had seven sacks,
+Each sack had seven cats,
+Each cat had seven kits.
+Kits, cats, sacks, and wives:
+How many were going to St. Ives?"""
+
+print(s)
+print(longstr)
+
+m=   s[0:7]
+
+print(m)
+n = longstr[36:39]
+print(n)
