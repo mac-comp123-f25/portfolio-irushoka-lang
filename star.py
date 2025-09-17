8a1a30549ab5f8b5466b806e3a6cda59293d9416
irushoka-lang <irushoka@macalester.edu>
2025-09-16 22:41:32 -0500

    Class activity

---
+++ star.py
@@ -0,0 +1,41 @@
+import turtle
+wn= turtle. Screen()
+Kary = turtle.Turtle()
+Kary.color("pink")
+Kary.shape("turtle")
+Kary.forward(50)
+Kary.right(144)
+Kary.forward(50)
+Kary.right(144)
+Kary.forward(50)
+Kary.right(144)
+Kary.forward(50)
+Kary.right(144)
+Kary.forward(50)
+Kary.right(144)
+Kary.forward(50)
+
+Bob = turtle.Turtle()
+Bob.color("orange")
+Bob.shape("turtle")
+Bob.up()
+Bob.goto(100,20)
+Bob.down()
+Bob.forward(50)
+Bob.right(144)
+Bob.forward(50)
+Bob.right(144)
+Bob.forward(50)
+Bob.right(144)
+Bob.forward(50)
+Bob.right(144)
+Bob.forward(50)
+Bob.right(144)
+
+
+
+
+
+
+
+
