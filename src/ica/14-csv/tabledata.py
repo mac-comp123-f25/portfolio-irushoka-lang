5c1fe1f339a54f25cb4f3182c9825bd4719024fd
irushoka-lang <irushoka@macalester.edu>
2025-10-27 10:40:59 -0500

    CSV ica

--- src/ica/14-csv/tabledata.py
+++ src/ica/14-csv/tabledata.py
@@ -9,6 +9,8 @@ table = [{'Name': 'Fox, Susan', 'Phone': '6553',
           'Building': 'Campus Center', 'OfficeNum': '399'},
 
          {'Name': 'Thimus, Reg', 'Phone': '6001',
+          'Building': 'Leonard', 'OfficeNum': '22'},
+         {'Name': 'Friedman, Maryam', 'Phone': '6001',
           'Building': 'Leonard', 'OfficeNum': '22'}]
 
 def read_csv(csv_filename):
@@ -31,10 +33,9 @@ def read_csv(csv_filename):
     return fields, table
 
 field_names, sun_table = read_csv("DataFiles/sunRiseSet.csv")
-print(field_names)
-print(sun_table[0])  # printing just the first row of data
-print(sun_table, field_names, 15)
-
+# print(field_names)
+# print(sun_table[0])  # printing just the first row of data
+# print(sun_table, field_names, 15)
 
 
 
@@ -47,4 +48,73 @@ def lookup_office(name, table):
 
     return "No entry" + name
 
-print(lookup_office('Fox, Susan', table))
+# print(lookup_office('Fox, Susan', table))
+
+
+
+
+def read_csv(csv_filename):
+    """
+    Build a list of dictionaries from a CSV file without converting numeric
+    values into integers.
+
+    :param csv_filename: the name of the CSV file to read
+    :return: list of field names (column headers in the CSV file) and
+    list of dictionaries holding the data (one dictionary for each row)
+    """
+    data_in = open(csv_filename, 'r')
+    csv_reader = csv.DictReader(data_in)
+    fields = csv_reader.fieldnames
+
+    tabular = []
+    for rowDict in csv_reader:
+        tabular.append(rowDict)
+    data_in.close()
+    return fields, tabular
+
+fieldNames, tabular = read_csv("DataFiles/sunRiseSet.csv")
+# print(tabular, fieldNames, 10)
+
+
+def lookup_by_date(monthname, monthday, tabular):
+    for row in tabular:
+        if row['Month'] == monthname and row['Day'] == monthday:
+            return  row
+
+
+# print(lookup_by_date('January', '10', tabular))
+
+
+def collect_by_building(capital_letter, table):
+    """
+    Given the name of a building, and a sunTable, make a list of all
+    the entries in the sunTable belonging to that building and return
+    that list
+    """
+    match_list = []
+
+    for row in table:
+        str = row['Name']
+        if str.startswith(capital_letter):
+            match_list.append(str)
+
+    return match_list
+
+# print(collect_by_building('F', table))
+
+
+
+def select_by_month(monthname,tabular):
+    empty_list = []
+    for row in tabular:
+        if row['Month'] == monthname:
+            empty_list.append(row)
+    return empty_list
+
+print(select_by_month("February",tabular))
+
+
+# def average_daylight_time(tabular):
+
+
+
